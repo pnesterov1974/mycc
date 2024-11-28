df23d15c7e9149f252fad75ee2b0f1daa630d131
pavel_nesterov <44893252+pnesterov1974@users.noreply.github.com>
2024-11-28 17:01:35 +0300

    Create doms.cs

---
+++ doms.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace import_kladr_ef.models;
+
+//[PrimaryKey(nameof(State), nameof(LicensePlate))]
+//[Table("DOMA", Schema = "dbo")]
+public class Doma
+{
+    [StringLength(40)]
+    public string NAME { get; set; }
+
+    [StringLength(10)]
+    public string KORP { get; set; }
+
+    [StringLength(10)]
+    public string SOCR { get; set; }
+
+    [Key]
+    [StringLength(19)]
+    public string CODE { get; set; }
+
+    [StringLength(6)]
+    public string INDEX { get; set; }
+
+    [StringLength(4)]
+    public string GNINMB { get; set; }
+
+    [StringLength(4)]
+    public string UNO { get; set; }
+
+    [StringLength(11)]
+    public string OCATD { get; set; }
+}
