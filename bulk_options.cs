1f9bea555f9257f9d18ffd303a7a23bebce7fa04
pavel_nesterov <44893252+pnesterov1974@users.noreply.github.com>
2024-12-05 17:14:20 +0300

    Create bulk_options.cs

---
+++ bulk_options.cs
@@ -0,0 +1,12 @@
+using (var loader = new SqlBulkCopy(connectionString, SqlBulkCopyOptions.Default))
+            {
+                loader.ColumnMappings.Add(0, 2);
+                loader.ColumnMappings.Add(1, 1);
+                loader.ColumnMappings.Add(2, 3);
+                loader.ColumnMappings.Add(3, 4);
+
+                loader.DestinationTableName = "Customers";
+                loader.WriteToServer(reader); 
+
+                Console.WriteLine("Загрузили!");
+            }
