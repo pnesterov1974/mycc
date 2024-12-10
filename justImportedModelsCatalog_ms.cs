410e8127610a70aa46c736395676efc97d1e1294
pavel_nesterov <44893252+pnesterov1974@users.noreply.github.com>
2024-12-10 16:07:17 +0300

    Create justImportedModelsCatalog_ms.cs

---
+++ justImportedModelsCatalog_ms.cs
@@ -0,0 +1,12 @@
+namespace Datareader.mssql.KladrJustImportedModels;
+public class KladrJustImportedCatalog
+{
+    public List<IReadModel> ReadModels = new List<IReadModel>
+    {
+        new SocrBaseJustImportedModel(),
+        new AltNamesJustImportedModel(),
+        new KladrJustImportedModel(),
+        new StreetJustImportedModel(),
+        new DomaJustImportedModel(),
+    };
+}
