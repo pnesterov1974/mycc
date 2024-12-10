7fa49b0fd772f23fde25d4d326640ed98b640150
pavel_nesterov <44893252+pnesterov1974@users.noreply.github.com>
2024-12-10 16:04:19 +0300

    Delete interfaces.cs

--- interfaces.cs
+++
@@ -1,8 +0,0 @@
-namespace Utils;
-
-public interface IDBUtils
-{
-    public bool ClearDestTable(string connectionString, string targetTableFullName);
-
-    public bool TryDbConnection(string connectionString);
-}
