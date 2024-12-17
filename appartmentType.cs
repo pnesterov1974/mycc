f532acb892ff48ada1c45f59b8bd84a5d234116a
pavel_nesterov <44893252+pnesterov1974@users.noreply.github.com>
2024-12-17 17:22:16 +0300

    Create appartmentType.cs

---
+++ appartmentType.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace importGeoEfc.models.garmodels;
+
+public class AppartmentType
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    public int Id { get; set; }
+
+    [StringLength(50)]
+    public string Name { get; set; }
+
+    [StringLength(50)]
+    public string? ShortName { get; set; }
+
+    [StringLength(250)]
+    public string? Desc { get; set; }
+    
+    public DateOnly UpdateDate { get; set; }
+    public DateOnly StartDate { get; set; }
+    public DateOnly EndDate { get; set; }
+    public bool IsActive { get; set; }
+}
